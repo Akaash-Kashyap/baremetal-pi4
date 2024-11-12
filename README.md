f6cead356598a81672681da66a0b2857bdb82a61
Akaash Kashyap <akaashkashyap@gmail.com>
2024-11-12 05:28:55 -0600

    got part 3 working

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # baremetal-pi4
 Learning how to write baremetal code on a raspberry pi
+
+Using guide from 
+https://www.rpi4os.com/
+
+Currently finished part 3
+
+TODO: read and understand the code we added in from io.c
--- kernel.c
+++ kernel.c
@@ -2,6 +2,7 @@
 
 // include
 #include "kernel.h"
+#include "io.h"
 
 volatile unsigned int tim;
 
@@ -9,15 +10,18 @@ volatile unsigned int tim;
 
 void main()
 {
-    GPIO_init();
-    // gpio[GPFSEL_4] = (1 << 6); // sets gpio 42 as output (led gpio)
-    GPIO_start(42,1);
-    while(1){
-        for(tim = 0; tim < 50000; tim++)
-            ;
-        ledOn();
-        for(tim = 0; tim < 50000; tim++)
-            ;
-        ledOff();
-    }
+    // GPIO_init();
+    // // gpio[GPFSEL_4] = (1 << 6); // sets gpio 42 as output (led gpio)
+    // GPIO_start(42,1);
+    // while(1){
+    //     for(tim = 0; tim < 50000; tim++)
+    //         ;
+    //     ledOn();
+    //     for(tim = 0; tim < 50000; tim++)
+    //         ;
+    //     ledOff();
+    // }
+    uart_init();
+    uart_writeText("Hello World!\n");
+    while(1);
 }
